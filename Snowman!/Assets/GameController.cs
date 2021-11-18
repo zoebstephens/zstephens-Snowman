b4604ccae61b36ca33d3545a9febd55a54a681bb
zoebstephens <zoebstephens@gmail.com>
2021-11-18 02:48:23 -0800

    Challenge 1

--- Snowman!/Assets/GameController.cs
+++ Snowman!/Assets/GameController.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameController : MonoBehavior
 {
 
     public UnityEngine.UI.Text Message;
-
     public UnityEngine.UI.Button StartButton;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
 
     public void StartGame()
     {
         this.Message.text = "Can you save the Snowman?";
         this.StartButton.gameObject.SetActive(false);
+        this.StartScreen.SetActive(false);
+        this.PlayScreen.SetActive(true);
+    }
+    
+    public void OpenStartScreen()
+    {
+        this.StartScreen.SetActive(false);
+        this.PlayScreen.SetActive(true);
     }
 
+    public void Start()
+    {
+        this.StartScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+    }
 }
 
